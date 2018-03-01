2fd129ba61c4d3598b6b38897186fbfdb6c1ae7c
Lloyd Briggs <lloydb1@gmail.com>
2018-03-01 10:17:48 -0800

    task(bootstrap morgan)

---
+++ config.js
@@ -0,0 +1,5 @@
+const path = require('path')
+
+module.exports = {
+  ACCESS_LOG: path.join(__dirname, 'var/log/access.log')
+}
--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-const express = require('express')
-    , db = require('./common/db')
-    , ApiController = require('./routes/api')
+const express         = require('express')
+    , fs              = require('fs')
+    , morgan          = require('morgan')
+    , path            = require('path')
+    , config          = require('../config')
+    , db              = require('./common/db')
+    , ApiController   = require('./routes/api')
 
+const accessLogStream = fs.createWriteStream(config.ACCESS_LOG, { flags: 'a' })
 const app = express()
+const isProduction = process.env.NODE_ENV === 'development'
 const port = process.env.PORT || 3000
 
+false === !!isProduction && app.use(morgan('dev'))
+true === !!isProduction && app.use(morgan('combined', { stream: accessLogStream }))
 app.use('/api', ApiController)
 app.listen(port, () => {
-    console.log(`server listening on port ${port}`)
+  console.log(`server listening on port ${port}`)
 })
